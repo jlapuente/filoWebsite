635918ed83d3b9223560f2bf4b9289fc53612e41
jlapuente <javierlapuentegarcia@gmail.com>
2021-01-22 17:24:02 +0100

    Nuevas graficas implementadas y optimizacion del codigo Incluido fichero de constantes Incluido nuevo calendario y boton de cambio

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faSync } from '@fortawesome/free-solid-svg-icons';
+declare var $: any;
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  faSync = faSync;
+  $: any;
   constructor() { }
 
   ngOnInit() {
+    // var btn = document.getElementById('my-btn');
+    // btn.onclick = function (event) {
+    //   btn.children[0].classList.add('spin-animation');
+    //   setTimeout(function () {
+    //     btn.children[0].classList.remove('spin-animation');
+    //   }, 500);
+    // }
+  }
+
+  animate(){
+    console.log($('.agenda'));
+    $('.agenda').toggle();
+    $('.birthday').toggle();
   }
 
 }
--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart } from 'node_modules/chart.js';
-
+import { CONSTANTS } from '../properties/CONSTANTS'
 
 
 @Component({
@@ -12,59 +12,35 @@ export class StatsComponent implements OnInit {
 
   labels: number[] = [];
   marks: number[] = [];
-  calculatedMarks: number[] = [];
+  calculatedLogicMarks: number[] = [];
+  calculatedPoliticsMarks: number[] = [];
+  calculatedIPFMarks: number[] = [];
   chart: any;
   ctx: any;
 
-  colors: string[] = [
-    'rgba(84, 71, 140, 0.6)',
-    'rgba(44, 105, 154, 0.6)',
-    'rgba(4, 139, 168, 0.6)',
-    'rgba(13, 179, 158, 0.6)',
-    'rgba(22, 219, 147, 0.6)',
-    'rgba(131, 227, 119, 0.6)',
-    'rgba(185, 231, 105, 0.6)',
-    'rgba(239, 234, 90, 0.6)',
-    'rgba(241, 196, 83, 0.6)',
-    'rgba(242, 158, 76, 0.6)',
-    'rgba(255, 56, 100, 0.6)'
-  ];
-  borderColors: string[] = [
-    'rgba(84, 71, 140, 1)',
-    'rgba(44, 105, 154, 1)',
-    'rgba(4, 139, 168, 1)',
-    'rgba(13, 179, 158, 1)',
-    'rgba(22, 219, 147, 1)',
-    'rgba(131, 227, 119, 1)',
-    'rgba(185, 231, 105, 1)',
-    'rgba(239, 234, 90, 1)',
-    'rgba(241, 196, 83, 1)',
-    'rgba(242, 158, 76, 1)',
-    'rgba(255, 56, 100, 1)'
-  ];
+  
+
   constructor() { }
 
   ngOnInit() {
     this.prepareLabels();
+    this.calculateMarks(this.calculatedPoliticsMarks, CONSTANTS.POLITICS_MARKS, document.getElementById('politicsChart'), CONSTANTS.TEN_ESCALE);
+    this.calculateMarks(this.calculatedIPFMarks, CONSTANTS.IPF_MARKS, document.getElementById('ipfChart'), CONSTANTS.SINGLE_ESCALE);
+    this.calculateMarks(this.calculatedLogicMarks, CONSTANTS.LOGIC_MARKS, document.getElementById('logicChart'), CONSTANTS.SINGLE_ESCALE);
   }
 
 
-  prepareGraph(){
-    let canva:any = document.getElementById('myChart');
+  prepareGraph(values, markLabel, canva, escale) {
     var ctx = canva.getContext('2d');
-    console.log(this.labels);
-    console.log(this.marks);
-    console.log(canva);
-    console.log(ctx);
     this.chart = new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: this.labels,
+        labels: markLabel,
         datasets: [{
           label: 'Notas',
-          data: this.calculatedMarks,
-          backgroundColor: this.colors,
-          borderColor: this.borderColors,
+          data: values,
+          backgroundColor: CONSTANTS.COLORS,
+          borderColor: CONSTANTS.BORDER_COLORS,
           borderWidth: 1
         }]
       },
@@ -73,7 +49,7 @@ export class StatsComponent implements OnInit {
           yAxes: [{
             ticks: {
               beginAtZero: true,
-              stepSize: 1
+              stepSize: escale
             }
           }]
         },
@@ -81,52 +57,29 @@ export class StatsComponent implements OnInit {
           display: false
         },
         tooltips: {
-          enabled: false
+          enabled: true
         }
       }
     });
-    console.log(this.chart);
   }
 
-  calculateMarks(){
-    this.marks.push(5); // Cristobal
-    this.marks.push(7); // Angel
-    this.marks.push(0); // Carlos
-    this.marks.push(10); // Yolanda
-    this.marks.push(0); // Carmen
-    this.marks.push(0); // Lara
-    this.marks.push(9); // Martin '5
-    this.marks.push(2); // Fer '5
-    this.marks.push(9); // Alex
-    this.marks.push(6); // Sofia '25
-    this.marks.push(7); // Javier '5
-    this.marks.push(8); // Capi '75
-    this.marks.push(9); // Jon 
-    this.marks.push(4); // Paola 
-    this.marks.push(8); // Mario '5
-    this.marks.push(0); // Eli 
-    this.marks.push(7); // Maria '5
-
-
-    for (let index = 0; index < this.marks.length; index++) {
-      this.calculatedMarks.push(this.getOccurrence(this.marks, index));
+  calculateMarks(emptyArray, marks, chart, escale) {
+    for (let index = 0; index < marks.length; index++) {
+      emptyArray.push(this.getOccurrence(marks, index));
     }
+    this.prepareGraph(emptyArray, this.labels, chart, escale);
 
-    this.prepareGraph();
   }
 
-  prepareLabels(){
+  prepareLabels() {
     for (let index = 0; index <= 10; index++) {
       this.labels.push(index);
     }
-    this.calculateMarks();
   }
 
- getOccurrence(array, value) {
-  var count = 0;
-  array.forEach((v) => (v === value && count++));
-  return count;
-}
-
-  
+  getOccurrence(array, value) {
+    var count = 0;
+    array.forEach((v) => (v === value && count++));
+    return count;
+  }
 }
